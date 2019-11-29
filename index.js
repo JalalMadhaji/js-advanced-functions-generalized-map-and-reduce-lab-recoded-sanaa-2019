a65d1eb9884a8bfec29d12fe2a505f31b88a78f2
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-11-29 18:08:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const map = function(arr,fn){
   return arr.map(fn);
 }
 const reduce = function(arr,callback,startingPoint){
+  if(startingPoint != null){
+    return arr.reduce(callback,startingPoint);
+  }
   return arr.reduce(callback);
 }
